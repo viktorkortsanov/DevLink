d9c9c7af6de5d8014f938ec79d6e0c18503e5c42
Viktor Kortsanov <viktorkortsanov@gmail.com>
2025-07-02 18:56:33 +0300

    Added auth component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Main } from './main/main';
+import { AuthComponent } from './user/auth/auth';
 
 export const routes: Routes = [
     { path: '', component: Main },
+    { path: 'sign-up', component: AuthComponent}
 ];
